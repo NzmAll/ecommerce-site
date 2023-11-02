3470ffb97f1e05ce52e40581b2466abba659b6fe
nzm.allahverdyv03@gmail.com <nzm.allahverdyv03@gmail.com>
2023-11-02 11:26:41 +0400

    Breadcrumb and product page added

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
+import { useParams } from "react-router-dom";
+import Breadcrum from "../Components/Breadcrums/Breadcrum";
 
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
-}
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
 
-export default Product
+  return (
+    <div>
+      <Breadcrum product={product} />
+    </div>
+  );
+};
+
+export default Product;
